6b34ee95e6ae6bf67e5a5ee289b3b65ee51f7223
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-11-19 00:39:57 +0000

    ✨feat(Components): Crear componente PrompText -Se crea PrompText, campo donde ser envia la solicitud -Se oculta .env

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,31 @@
-import { Text, View } from "react-native";
+import { Button } from "@/components/Button";
+import { PromptText } from "@/components/PromptText";
+import { ResponseBox } from "@/components/ResponseBox";
+import '@/global.css';
+import axios from "axios";
+import { useState } from "react";
+import { KeyboardAvoidingView, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
+const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;//APikeyde google desde .env
+  const ai = new <GoogleGenAI({ apiKey: API_KEY });
+  const consultarGemini = async (pregunta: string) => {
+    setIsLoading(true);
+    await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: pregunta,
+    }).then((res) => {
+        if(res.text != undefined){
+          setResponse(res.text);
+        }else{
+          setResponse("No se pudo obtener la respuesta");
+        }
+    }).catch((err) => {
+      console.log(err);
+    }).finally(() => {
+      setIsLoading(false);
+    });
+}
   return (
     <View
       style={{
